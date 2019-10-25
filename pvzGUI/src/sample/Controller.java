5e18b1840d347657eba8bf0656406dd86510d12a
ArkaSarkar19 <arka18222@iiitd.ac.in>
2019-10-25 14:40:29 +0530

    changes made

--- pvzGUI/src/sample/Controller.java
+++ pvzGUI/src/sample/Controller.java
@@ -1,8 +1,18 @@
 package sample;
 
+import javafx.scene.control.Button;
+
 public class Controller {
-    public void print(){
-        System.out.println("Rachit sir pro hai");
+    public Button clickToStart;
+    public void handleStart(){
+//        clickToStart.getStyleClass().remove("clickToStart");
+//        clickToStart.getStyleClass().add("clickedStart");
+//        double wait = System.currentTimeMillis();
+//        while(System.currentTimeMillis()-wait<1000){
+//            ;}
+//        clickToStart.getStyleClass().remove("clickedStart");
+//        clickToStart.getStyleClass().add("clickToStart");
+        System.out.println(21345678);
     }
 
 }
--- pvzGUI/src/sample/Main.java
+++ pvzGUI/src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("startpage.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1080, 810));
         primaryStage.setResizable(false);
