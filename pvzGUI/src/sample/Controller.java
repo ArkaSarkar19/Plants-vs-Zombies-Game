87d40bb5c9b95439377e072d6f73ac15f402e7fc
ArkaSarkar19 <arka18222@iiitd.ac.in>
2019-10-26 01:37:27 +0530

    organised everything

--- pvzGUI/src/sample/Controller.java
+++ pvzGUI/src/sample/Controller.java
@@ -1,10 +1,6 @@
 package sample;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Button;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
