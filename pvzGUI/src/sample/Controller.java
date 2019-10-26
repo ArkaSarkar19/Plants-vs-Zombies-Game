b0eaf1b79fb2abd63821f512a8717e62583bc527
ArkaSarkar19 <arka18222@iiitd.ac.in>
2019-10-26 11:48:29 +0530

    levelscreen and some animation added

--- pvzGUI/src/sample/Controller.java
+++ pvzGUI/src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
@@ -10,6 +11,7 @@ import java.time.Duration;
 public class Controller {
     public Button clickToSignup;
     public Button clickToStart;
+    public Button clickToLogin;
     public ImageView textonlevelscreen;
     public void handleStart() throws IOException {
        LoginBox.getLogin();
@@ -20,12 +22,18 @@ public class Controller {
     public void handleleveltexttransition(){
         FadeTransition ft = new FadeTransition();
         ft.setNode(textonlevelscreen);
-        ft.setDuration(new Duration(0.5);
+       // ft.setDuration(new Duration(0.5));
         ft.setFromValue(1.0);
         ft.setToValue(0.0);
-        ft.setCycleCount(6);
+        ft.setCycleCount(Timeline.INDEFINITE);
         ft.setAutoReverse(true);
+        ft.playFromStart();
 
+    }
+    public void handleLogin() throws IOException {
+
+        LoginBox.getLevelpage();
+        LoginBox.loginwindow.close();
     }
 
 }
--- pvzGUI/src/sample/LoginBox.java
+++ pvzGUI/src/sample/LoginBox.java
@@ -9,25 +9,40 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LoginBox {
+    public static Stage loginwindow;
+    public static Stage signupwindow;
+    public static Stage levelwindow;
+
+
     public static void getLogin() throws IOException {
-        Stage window = new Stage();
-        window.initModality(Modality.APPLICATION_MODAL);
+        loginwindow = new Stage();
+        loginwindow.initModality(Modality.APPLICATION_MODAL);
         Parent login = FXMLLoader.load(LoginBox.class.getResource("LoginBox.fxml"));
         Scene scene1 = new Scene(login, 720,405);
-        window.setScene(scene1);
-        window.setTitle("Login");
-        window.setResizable(false);
-        window.showAndWait();
+        loginwindow.setScene(scene1);
+        loginwindow.setTitle("Login");
+        loginwindow.setResizable(false);
+        loginwindow.showAndWait();
     }
     public static void getSignup() throws IOException {
-        Stage window = new Stage();
-        window.initModality(Modality.APPLICATION_MODAL);
+        signupwindow = new Stage();
+        signupwindow.initModality(Modality.APPLICATION_MODAL);
         Parent login = FXMLLoader.load(LoginBox.class.getResource("Signup.fxml"));
         Scene scene1 = new Scene(login);
-        window.setScene(scene1);
-        window.setTitle("Register");
-        window.setResizable(false);
-        window.showAndWait();
+        signupwindow.setScene(scene1);
+        signupwindow.setTitle("Register");
+        signupwindow.setResizable(false);
+        signupwindow.showAndWait();
+    }
+    public static void getLevelpage() throws IOException {
+        levelwindow = new Stage();
+        levelwindow.initModality(Modality.APPLICATION_MODAL);
+        Parent login = FXMLLoader.load(LoginBox.class.getResource("SelectLevel.fxml"));
+        Scene scene1 = new Scene(login);
+        levelwindow.setScene(scene1);
+        levelwindow.setTitle("Select Level");
+        levelwindow.setResizable(false);
+        levelwindow.show();
     }
 }
 
