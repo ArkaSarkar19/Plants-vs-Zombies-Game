e81ee6534c4dbdd6fd08dfc9d8fa66fe64e27ed7
xrachitx <rachit18302@iiitd.ac.in>
2019-11-29 22:46:12 +0530

    patani kya change kiya

--- pvzGUI/src/sample/Zombie.java
+++ pvzGUI/src/sample/Zombie.java
@@ -150,7 +150,6 @@ public class Zombie  extends Character implements Comparable<Zombie>{
             }
             tt.setByX(-speed);
             tt.playFromStart();
-//            System.out.println(tt.getByX());
         });
     }
     public boolean gameOver(){
